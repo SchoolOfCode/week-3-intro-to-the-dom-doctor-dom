e155050a4d181c7f15f0926e6b7703d8c5092ae5
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-09 12:29:56 +0000

    ticket 1 completed

---
+++ index.js
@@ -0,0 +1,9 @@
+// Instructions:
+// Use getElementById() to select the element with the id of "title".
+// Log the selected element to the console.
+
+console.log(document.getElementById("title"));
+console.log(document.getElementsByClassName("content"));
+console.log(document.getElementsByTagName("li"));
+console.log(document.querySelector(".highlight"));
+console.log(document.querySelectorAll(".highlight"));
