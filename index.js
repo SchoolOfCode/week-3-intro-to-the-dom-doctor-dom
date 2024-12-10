25c34e8838e568f0f7d3c56dff9d22a7f0dd7948
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-10 22:43:35 +0000

    ticket 1a-1e completed

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-// Instructions:
+console.log("Linked!");
+
+// Ticket 1a: getElementById()
 // Use getElementById() to select the element with the id of "title".
 // Log the selected element to the console.
 
 console.log(document.getElementById("title"));
-console.log(document.getElementsByClassName("content"));
-console.log(document.getElementsByTagName("li"));
+
+// Ticket 1b: getElementsByClassName()
+// Use getElementsByClassName() to select elements with the class of "content".
+// Log the selected elements to the console.
+
+const etwas = document.getElementsByClassName("content");
+
+for (let i = 0; i < etwas.length; i++) {
+    console.log(etwas.item(i));
+}
+
+// Ticket 1c: getElementsByTagName()
+// Use getElementsByTagName() to select all <li> elements on the page.
+// Log the selected elements to the console.
+
+const elements = document.getElementsByTagName("li");
+
+for (let i = 0; i < elements.length; i++) {
+    console.log(elements.item(i));
+}
+
+// Ticket 1d: querySelector()
+// Use querySelector() to select the first element with the class "highlight".
+// Log the selected element to the console.
+
 console.log(document.querySelector(".highlight"));
-console.log(document.querySelectorAll(".highlight"));
+
+// Ticket 1e: querySelectorAll()
+// Use querySelectorAll() to select all elements with the class "highlight".
+// Log the selected elements to the console.
+
+const highlight = document.querySelectorAll(".highlight");
+
+for (let i = 0; i < highlight.length; i++) {
+    console.log(highlight.item(i));
+}
